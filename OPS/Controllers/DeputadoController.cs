c3a76bf7e795c39c0cf9bad4ac8d876b45775760
Vanderlei Denir <vanderleidenir@hotmail.com>
2019-11-17 13:00:32 -0300

    Tratamento para exibir imagem padrão quando a mesma não existe

--- OPS/Controllers/DeputadoController.cs
+++ OPS/Controllers/DeputadoController.cs
@@ -1,7 +1,10 @@
 ﻿using AspNetCore.CacheOutput;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using OPS.Core.DAO;
 using OPS.Core.DTO;
+using System;
 using System.Threading.Tasks;
 
 namespace OPS.WebApi
@@ -10,132 +13,154 @@ namespace OPS.WebApi
     [Route("api/[controller]")]
     [CacheOutput(ServerTimeSpan = 43200 /* 12h */)]
     public class DeputadoController : Controller
-	{
-		DeputadoDao dao;
+    {
+        private IHostingEnvironment Environment { get; }
+        private IConfiguration Configuration { get; }
 
-		public DeputadoController()
-		{
-			dao = new DeputadoDao();
-		}
+        DeputadoDao dao;
 
-		[HttpGet("{id:int}")]
-		public async Task<dynamic> Consultar(int id)
-		{
+        public DeputadoController(IConfiguration configuration, IHostingEnvironment env)
+        {
+            Environment = env;
+            Configuration = configuration;
+
+            dao = new DeputadoDao();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<dynamic> Consultar(int id)
+        {
             return await dao.Consultar(id);
-		}
+        }
 
-		[HttpPost("Lista")]
-		[CacheOutput(ClientTimeSpan = 43200 /* 12h */, ServerTimeSpan = 43200 /* 12h */)]
-		public async Task<dynamic> Lista(FiltroParlamentarDTO filtro)
-		{
+        [HttpPost("Lista")]
+        [CacheOutput(ClientTimeSpan = 43200 /* 12h */, ServerTimeSpan = 43200 /* 12h */)]
+        public async Task<dynamic> Lista(FiltroParlamentarDTO filtro)
+        {
             return await dao.Lista(filtro);
-		}
+        }
 
-		[HttpGet("Pesquisa")]
-		[CacheOutput(ClientTimeSpan = 43200 /* 12h */, ServerTimeSpan = 43200 /* 12h */)]
-		public async Task<dynamic> Pesquisa()
-		{
+        [HttpGet("Pesquisa")]
+        [CacheOutput(ClientTimeSpan = 43200 /* 12h */, ServerTimeSpan = 43200 /* 12h */)]
+        public async Task<dynamic> Pesquisa()
+        {
             return await dao.Pesquisa();
-		}
+        }
 
-		[HttpGet("Lancamentos")]
-		public async Task<dynamic> Lancamentos([FromQuery]FiltroParlamentarDTO filtro)
-		{
+        [HttpGet("Lancamentos")]
+        public async Task<dynamic> Lancamentos([FromQuery]FiltroParlamentarDTO filtro)
+        {
             return await dao.Lancamentos(filtro);
-		}
+        }
 
-		[HttpGet("TipoDespesa")]
-		[CacheOutput(ClientTimeSpan = 43200 /* 12h */, ServerTimeSpan = 43200 /* 12h */)]
-		public async Task<dynamic> TipoDespesa()
-		{
-			return await dao.TipoDespesa();
-		}
+        [HttpGet("TipoDespesa")]
+        [CacheOutput(ClientTimeSpan = 43200 /* 12h */, ServerTimeSpan = 43200 /* 12h */)]
+        public async Task<dynamic> TipoDespesa()
+        {
+            return await dao.TipoDespesa();
+        }
 
-		[HttpGet("Secretarios")]
-		public async Task<dynamic> Secretarios([FromQuery]FiltroParlamentarDTO filtro)
-		{
+        [HttpGet("Secretarios")]
+        public async Task<dynamic> Secretarios([FromQuery]FiltroParlamentarDTO filtro)
+        {
             return await dao.Secretarios(filtro);
-		}
+        }
 
-		[HttpGet("{id:int}/Secretarios")]
-		public async Task<dynamic> SecretariosPorDeputado(int id)
-		{
+        [HttpGet("{id:int}/Secretarios")]
+        public async Task<dynamic> SecretariosPorDeputado(int id)
+        {
             return await dao.SecretariosPorDeputado(id);
-		}
+        }
 
-		[HttpGet("{id:int}/GastosMensaisPorAno")]
-		public async Task<dynamic> GastosMensaisPorAno(int id)
-		{
+        [HttpGet("{id:int}/GastosMensaisPorAno")]
+        public async Task<dynamic> GastosMensaisPorAno(int id)
+        {
             return await dao.GastosMensaisPorAno(id);
-		}
+        }
 
-		[HttpGet("Documento/{id:int}")]
-		public async Task<dynamic> Documento(int id)
-		{
-			var result = await dao.Documento(id);
+        [HttpGet("Documento/{id:int}")]
+        public async Task<dynamic> Documento(int id)
+        {
+            var result = await dao.Documento(id);
 
-		    if (result != null)
-		        return Ok(result);
-		    else
-		        return NotFound();
-		}
+            if (result != null)
+                return Ok(result);
+            else
+                return NotFound();
+        }
 
-		[HttpGet("DocumentosDoMesmoDia/{id:int}")]
-		public async Task<dynamic> DocumentosDoMesmoDia(int id)
-		{
-			var result = await dao.DocumentosDoMesmoDia(id);
+        [HttpGet("DocumentosDoMesmoDia/{id:int}")]
+        public async Task<dynamic> DocumentosDoMesmoDia(int id)
+        {
+            var result = await dao.DocumentosDoMesmoDia(id);
 
-			return Ok(result);
-		}
+            return Ok(result);
+        }
 
-		[HttpGet("DocumentosDaSubcotaMes/{id:int}")]
-		public async Task<dynamic> DocumentosDaSubcotaMes(int id)
-		{
-			var result = await dao.DocumentosDaSubcotaMes(id);
+        [HttpGet("DocumentosDaSubcotaMes/{id:int}")]
+        public async Task<dynamic> DocumentosDaSubcotaMes(int id)
+        {
+            var result = await dao.DocumentosDaSubcotaMes(id);
 
-			return Ok(result);
-		}
+            return Ok(result);
+        }
 
-		[HttpGet("{id:int}/MaioresNotas")]
-		public async Task<dynamic> MaioresNotas(int id)
-		{
+        [HttpGet("{id:int}/MaioresNotas")]
+        public async Task<dynamic> MaioresNotas(int id)
+        {
             return await dao.MaioresNotas(id);
-		}
+        }
 
-		[HttpGet("{id:int}/MaioresFornecedores")]
-		public async Task<dynamic> MaioresFornecedores(int id)
-		{
+        [HttpGet("{id:int}/MaioresFornecedores")]
+        public async Task<dynamic> MaioresFornecedores(int id)
+        {
             return await dao.MaioresFornecedores(id);
-		}
+        }
 
-		[HttpGet("{id:int}/ResumoPresenca")]
-		public async Task<dynamic> ResumoPresenca(int id)
-		{
+        [HttpGet("{id:int}/ResumoPresenca")]
+        public async Task<dynamic> ResumoPresenca(int id)
+        {
             return await dao.ResumoPresenca(id);
-		}
+        }
 
-		[HttpGet("CamaraResumoMensal")]
-		public async Task<dynamic> CamaraResumoMensal()
-		{
+        [HttpGet("CamaraResumoMensal")]
+        public async Task<dynamic> CamaraResumoMensal()
+        {
             return await dao.CamaraResumoMensal();
-		}
+        }
 
-		[HttpGet("CamaraResumoAnual")]
-		public async Task<dynamic> CamaraResumoAnual()
-		{
+        [HttpGet("CamaraResumoAnual")]
+        public async Task<dynamic> CamaraResumoAnual()
+        {
             return await dao.CamaraResumoAnual();
-		}
+        }
 
-		[HttpGet("Frequencia/{id:int}")]
-		public async Task<dynamic> Frequencia(int id)
-		{
+        [HttpGet("Frequencia/{id:int}")]
+        public async Task<dynamic> Frequencia(int id)
+        {
             return await dao.Frequencia(id);
-		}
+        }
 
-		[HttpGet("Frequencia")]
-		public async Task<dynamic> Frequencia([FromQuery]FiltroFrequenciaCamaraDTO filtro)
-		{
-			return await dao.Frequencia(filtro);
-		}
-	}
+        [HttpGet("Frequencia")]
+        public async Task<dynamic> Frequencia([FromQuery]FiltroFrequenciaCamaraDTO filtro)
+        {
+            return await dao.Frequencia(filtro);
+        }
+
+        [HttpGet("Imagem/{id}")]
+        public VirtualFileResult Imagem(string id)
+        {
+            var file = @"images/Parlamentares/DEPFEDERAL/" + id + ".jpg";
+            var filePath = System.IO.Path.Combine(Environment.WebRootPath, file);
+
+            if (System.IO.File.Exists(filePath))
+            {
+                return File(file, "image/jpeg");
+            }
+            else
+            {
+                return File(@"images/sem_foto.jpg", "image/jpeg");
+            }
+        }
+    }
 }
--- OPS/Controllers/SenadorController.cs
+++ OPS/Controllers/SenadorController.cs
@@ -1,5 +1,7 @@
 ﻿using AspNetCore.CacheOutput;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using OPS.Core.DAO;
 using OPS.Core.DTO;
 
@@ -10,83 +12,106 @@ namespace OPS.WebApi
     [CacheOutput(ServerTimeSpan = 43200 /* 12h */)]
     public class SenadorController : Controller
     {
-		SenadorDao dao;
 
-		public SenadorController()
-		{
-			dao = new SenadorDao();
-		}
+        private IHostingEnvironment Environment { get; }
+        private IConfiguration Configuration { get; }
 
-		[HttpGet]
-		[Route("{id:int}")]
-		public dynamic Consultar(int id)
-		{
-			return dao.Consultar(id);
-		}
+        SenadorDao dao;
 
-		[HttpGet]
-		[Route("")]
-		[CacheOutput(ClientTimeSpan = 43200 /* 12h */, ServerTimeSpan = 43200 /* 12h */)]
+        public SenadorController(IConfiguration configuration, IHostingEnvironment env)
+        {
+            Environment = env;
+            Configuration = configuration;
+
+            dao = new SenadorDao();
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public dynamic Consultar(int id)
+        {
+            return dao.Consultar(id);
+        }
+
+        [HttpGet]
+        [Route("")]
+        [CacheOutput(ClientTimeSpan = 43200 /* 12h */, ServerTimeSpan = 43200 /* 12h */)]
         public dynamic Pesquisa()
-		{
-			return dao.Pesquisa();
-		}
+        {
+            return dao.Pesquisa();
+        }
 
-		[HttpGet]
-		[Route("Lancamentos")]
-		public dynamic Lancamentos([FromQuery]FiltroParlamentarDTO filtro)
-		{
-			return dao.Lancamentos(filtro);
-		}
+        [HttpGet]
+        [Route("Lancamentos")]
+        public dynamic Lancamentos([FromQuery]FiltroParlamentarDTO filtro)
+        {
+            return dao.Lancamentos(filtro);
+        }
 
-		[HttpGet]
-		[Route("TipoDespesa")]
-		[CacheOutput(ClientTimeSpan = 43200 /* 12h */, ServerTimeSpan = 43200 /* 12h */)]
+        [HttpGet]
+        [Route("TipoDespesa")]
+        [CacheOutput(ClientTimeSpan = 43200 /* 12h */, ServerTimeSpan = 43200 /* 12h */)]
         public dynamic TipoDespesa()
-		{
-			return dao.TipoDespesa();
-		}
+        {
+            return dao.TipoDespesa();
+        }
 
-		[HttpGet]
-		[Route("{id:int}/GastosMensaisPorAno")]
-		public dynamic GastosMensaisPorAno(int id)
-		{
-			return dao.GastosMensaisPorAno(id);
-		}
+        [HttpGet]
+        [Route("{id:int}/GastosMensaisPorAno")]
+        public dynamic GastosMensaisPorAno(int id)
+        {
+            return dao.GastosMensaisPorAno(id);
+        }
 
-		//[HttpGet]
-		//[Route("Documento/{id:int}")]
-		//public dynamic Documento(int id)
-		//{
-		//	return dao.Documento(id);
-		//}
+        //[HttpGet]
+        //[Route("Documento/{id:int}")]
+        //public dynamic Documento(int id)
+        //{
+        //	return dao.Documento(id);
+        //}
 
-		[HttpGet]
-		[Route("{id:int}/MaioresNotas")]
-		public dynamic MaioresNotas(int id)
-		{
-			return dao.MaioresNotas(id);
-		}
+        [HttpGet]
+        [Route("{id:int}/MaioresNotas")]
+        public dynamic MaioresNotas(int id)
+        {
+            return dao.MaioresNotas(id);
+        }
 
-		[HttpGet]
-		[Route("{id:int}/MaioresFornecedores")]
-		public dynamic MaioresFornecedores(int id)
-		{
-			return dao.MaioresFornecedores(id);
-		}
+        [HttpGet]
+        [Route("{id:int}/MaioresFornecedores")]
+        public dynamic MaioresFornecedores(int id)
+        {
+            return dao.MaioresFornecedores(id);
+        }
 
-	    [HttpGet]
-	    [Route("SenadoResumoMensal")]
-	    public dynamic SenadoResumoMensal()
-	    {
-		    return dao.SenadoResumoMensal();
-	    }
+        [HttpGet]
+        [Route("SenadoResumoMensal")]
+        public dynamic SenadoResumoMensal()
+        {
+            return dao.SenadoResumoMensal();
+        }
 
-	    [HttpGet]
-	    [Route("SenadoResumoAnual")]
-	    public dynamic SenadoResumoAnual()
-	    {
-		    return dao.SenadoResumoAnual();
-	    }
-	}
+        [HttpGet]
+        [Route("SenadoResumoAnual")]
+        public dynamic SenadoResumoAnual()
+        {
+            return dao.SenadoResumoAnual();
+        }
+
+        [HttpGet("Imagem/{id}")]
+        public VirtualFileResult Imagem(string id)
+        {
+            var file = @"images/Parlamentares/SENADOR/" + id + ".jpg";
+            var filePath = System.IO.Path.Combine(Environment.WebRootPath, file);
+
+            if (System.IO.File.Exists(filePath))
+            {
+                return File(file, "image/jpeg");
+            }
+            else
+            {
+                return File(@"images/sem_foto.jpg", "image/jpeg");
+            }
+        }
+    }
 }
