e28d56ac3004d51fde2e917ce1cc81aea2071fe5
Vanderlei Denir <vanderleidenir@hotmail.com>
2021-01-24 17:23:18 -0300

    Adicionar sigla do partido a lista para facilitar a pesquisa;

--- OPS.Core/DAO/PartidoDao.cs
+++ OPS.Core/DAO/PartidoDao.cs
@@ -4,28 +4,28 @@ using System.Threading.Tasks;
 
 namespace OPS.Core.DAO
 {
-	public class PartidoDao
-	{
-		public async Task<dynamic> Consultar()
-		{
-			using (AppDb banco = new AppDb())
-			{
-				var lstRetorno = new List<dynamic>();
+    public class PartidoDao
+    {
+        public async Task<dynamic> Consultar()
+        {
+            using (AppDb banco = new AppDb())
+            {
+                var lstRetorno = new List<dynamic>();
 
-				DbDataReader reader = await banco.ExecuteReaderAsync("SELECT id, sigla, nome FROM partido order by nome;");
-			    while (await reader.ReadAsync())
+                DbDataReader reader = await banco.ExecuteReaderAsync("SELECT id, sigla, nome FROM partido order by nome;");
+                while (await reader.ReadAsync())
                 {
-					lstRetorno.Add(new
-					{
-						id = reader.GetFieldValue<uint>(0),
-						tokens = new[] { reader.GetFieldValue<string>(1), reader.GetFieldValue<string>(2) },
-						text = reader.GetFieldValue<string>(2)
-					});
-				}
-			    reader.Close();
+                    lstRetorno.Add(new
+                    {
+                        id = reader.GetFieldValue<uint>(0),
+                        tokens = new[] { reader.GetFieldValue<string>(1) },
+                        text = string.Format("{0} ({1})", reader.GetFieldValue<string>(2), reader.GetFieldValue<string>(1))
+                    });
+                }
+                reader.Close();
 
                 return lstRetorno;
-			}
-		}
-	}
+            }
+        }
+    }
 }
